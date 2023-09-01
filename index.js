1f268757d93d57751916662ff16422532c40e8eb
victorpinhors <pinho.victor@gmail.com>
2023-09-01 11:39:59 -0300

    Banco na nuvem e porta atualizada

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
 // const url = "mongodb://localhost:27017";
-const url = "mongodb://127.0.0.1:27017";
+// const url = "mongodb://127.0.0.1:27017";
+const url = "mongodb+srv://admin:U0RUJOWEwut4Alae@cluster0.244p2pq.mongodb.net";
 // const url = "mongodb+srv://admin:V90K7ehx2krw7OlM@cluster0.gbnr4oi.mongodb.net";
 const dbName = "jornada-backend-agosto-23";
 const client = new MongoClient(url);
@@ -93,7 +94,7 @@ async function main() {
     res.status(204).send();
   });
 
-  app.listen(3000);
+  app.listen(process.env.PORT || 3000);
 }
 
 main();
